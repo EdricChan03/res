6e420f9ea1fd5b8e43072e61791e1f48521d1a9b
Edric Chan <edric.chan.1997@gmail.com>
2017-11-14 11:38:03 +0800

    feat: Add colours to echo

--- scripts/deploy-github-pages.sh
+++ scripts/deploy-github-pages.sh
@@ -1,14 +1,18 @@
 #!/usr/local/bin/bash
 
 # Builds the app in production mode
-echo ---------------------------
-echo \| Building application... \|
-echo ---------------------------
+echo -e "\e[32m---------------------------"
+echo -e "|                         |"
+echo -e "| Building application... |"
+echo -e "|                         |"
+echo -e "---------------------------\e[0m"
 ng build --prod --base-href /res/ --deploy-url /res/
 
-echo --------------------
-echo \| Copying files... \|
-echo --------------------
+echo -e "\e[32m--------------------"
+echo -e "|                  |"
+echo -e "| Copying files... |"
+echo -e "|                  |"
+echo -e "--------------------\e[0m"
 # Copies `mdi.svg`, `material-light.css` and `material-dark.css` to the dist folder
 cp ./src/assets/{mdi.svg,material-light.css,material-dark.css} dist/
 # Deploys the app to Github pages
