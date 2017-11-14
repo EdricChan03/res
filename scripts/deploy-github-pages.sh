7cb80a593918463fa3772d8bd91b4db14fc8c5c9
Edric Chan <edric.chan.1997@gmail.com>
2017-11-14 11:32:24 +0800

    chore(deploy-ghp): Add echo

--- scripts/deploy-github-pages.sh
+++ scripts/deploy-github-pages.sh
@@ -1,10 +1,16 @@
 #!/usr/local/bin/bash
 
 # Builds the app in production mode
+echo ---------------------------
+echo | Building application... |
+echo ---------------------------
 ng build --prod --base-href /res/ --deploy-url /res/
 
-# Copies `mdi.svg` to the dist folder
-cp ./src/assets/mdi.svg dist/
+echo --------------------
+echo | Copying files... |
+echo --------------------
+# Copies `mdi.svg`, `material-light.css` and `material-dark.css` to the dist folder
+cp ./src/assets/{mdi.svg,material-light.css,material-dark.css} dist/
 # Deploys the app to Github pages
 ngh
 # Removes the dist directory
