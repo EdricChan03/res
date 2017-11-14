125b310f12d44edb3e60b3d7c89046e5d678e765
Edric Chan <edric.chan.1997@gmail.com>
2017-11-14 11:43:21 +0800

    refactor(deploy-ghp): Use numbers instead

--- scripts/deploy-github-pages.sh
+++ scripts/deploy-github-pages.sh
@@ -2,19 +2,19 @@
 
 # Builds the app in production mode
 echo -e "\e[32m---------------------------"
-echo -e "| Building application... |"
+echo -e "| 1. Building application |"
 echo -e "---------------------------\e[0m"
 ng build --prod --base-href /res/ --deploy-url /res/
 
 # Copies `mdi.svg`, `material-light.css` and `material-dark.css` to the dist folder
 echo -e "\e[32m--------------------"
-echo -e "| Copying files... |"
+echo -e "| 2. Copying files |"
 echo -e "--------------------\e[0m"
 cp ./src/assets/{mdi.svg,material-light.css,material-dark.css} dist/
 
 # Deploys the app to Github pages
 echo -e "\e[32m--------------------------------"
-echo -e "| Deploying to Github Pages... |"
+echo -e "| 3. Deploying to Github Pages |"
 echo -e "--------------------------------\e[0m"
 ngh
 # Removes the dist directory
