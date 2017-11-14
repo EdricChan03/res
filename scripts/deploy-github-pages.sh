64688c0243fcd1a2a512428b2d3590dbbf348979
Edric Chan <edric.chan.1997@gmail.com>
2017-11-14 11:41:23 +0800

    chore(deploy-ghp): Remove not needed lines

--- scripts/deploy-github-pages.sh
+++ scripts/deploy-github-pages.sh
@@ -2,20 +2,20 @@
 
 # Builds the app in production mode
 echo -e "\e[32m---------------------------"
-echo -e "|                         |"
 echo -e "| Building application... |"
-echo -e "|                         |"
 echo -e "---------------------------\e[0m"
 ng build --prod --base-href /res/ --deploy-url /res/
 
-echo -e "\e[32m--------------------"
-echo -e "|                  |"
-echo -e "| Copying files... |"
-echo -e "|                  |"
-echo -e "--------------------\e[0m"
 # Copies `mdi.svg`, `material-light.css` and `material-dark.css` to the dist folder
+echo -e "\e[32m--------------------"
+echo -e "| Copying files... |"
+echo -e "--------------------\e[0m"
 cp ./src/assets/{mdi.svg,material-light.css,material-dark.css} dist/
+
 # Deploys the app to Github pages
+echo -e "\e[32m--------------------------------"
+echo -e "| Deploying to Github Pages... |"
+echo -e "--------------------------------\e[0m"
 ngh
 # Removes the dist directory
 rm -rf dist/
