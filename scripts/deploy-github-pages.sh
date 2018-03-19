ac2069b6b3c6575e64be07b99154ec5fff0ee819
Edric Chan <edric.chan.1997@gmail.com>
2018-03-19 15:20:41 +0800

    chore(deploy-gh): add generating icons section

--- scripts/deploy-github-pages.sh
+++ scripts/deploy-github-pages.sh
@@ -1,22 +1,25 @@
 #!/usr/local/bin/bash
 
 CLICOLOR=1
-echo -e "\e[1m\e[33mEnabling colour...\e[0m"
+echo -e "\e[32m-----------------------"
+echo -e "| 1. Generating icons |"
+echo -e "-----------------------\e[0m"
+gulp default-icons
 # Builds the app in production mode
 echo -e "\e[32m---------------------------"
-echo -e "| 1. Building application |"
+echo -e "| 2. Building application |"
 echo -e "---------------------------\e[0m"
 ng build --prod --base-href /res/ --deploy-url /res/
 
 # Copies `mdi.svg`, `material-light.css` and `material-dark.css` to the dist folder
 echo -e "\e[32m--------------------"
-echo -e "| 2. Copying files |"
+echo -e "| 3. Copying files |"
 echo -e "--------------------\e[0m"
 cp ./src/assets/{mdi.svg,material-light.css,material-dark.css,init-blogger.js} dist/
 
 # Deploys the app to Github pages
 echo -e "\e[32m--------------------------------"
-echo -e "| 3. Deploying to Github Pages |"
+echo -e "| 4. Deploying to Github Pages |"
 echo -e "--------------------------------\e[0m"
 ngh
 # Removes the dist directory
