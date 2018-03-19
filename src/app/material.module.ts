e1523de80390d5b827b28c55ca3585217b8abada
Edric Chan <edric.chan.1997@gmail.com>
2018-03-19 15:23:26 +0800

    chore(material): add expansion module

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -6,6 +6,7 @@ import { MatCardModule } from '@angular/material/card';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatDialogModule } from '@angular/material/dialog';
+import { MatExpansionModule } from '@angular/material/expansion';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule, MatIconRegistry } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
@@ -24,38 +25,39 @@ import { DomSanitizer } from '@angular/platform-browser';
 //#endregion
 //#region Material modules required by the app
 const MATERIAL_MODULES = [
-    MatButtonModule,
-    MatCardModule,
-    MatCheckboxModule,
-    MatDialogModule,
-    MatFormFieldModule,
-    MatIconModule,
-    MatInputModule,
-    MatListModule,
-    MatMenuModule,
-    MatProgressSpinnerModule,
-    MatSelectModule,
-    MatSnackBarModule,
-    MatSidenavModule,
-    MatToolbarModule,
-    MatTooltipModule
+	MatButtonModule,
+	MatCardModule,
+	MatCheckboxModule,
+	MatDialogModule,
+	MatExpansionModule,
+	MatFormFieldModule,
+	MatIconModule,
+	MatInputModule,
+	MatListModule,
+	MatMenuModule,
+	MatProgressSpinnerModule,
+	MatSelectModule,
+	MatSnackBarModule,
+	MatSidenavModule,
+	MatToolbarModule,
+	MatTooltipModule
 ];
 //#endregion
 //#region CDK modules required by the app
 const CDK_MODULES = [
-    LayoutModule
+	LayoutModule
 ];
 //#endregion
 @NgModule({
-    exports: [
-        MATERIAL_MODULES,
-        CDK_MODULES
-    ]
+	exports: [
+		MATERIAL_MODULES,
+		CDK_MODULES
+	]
 })
 export class MaterialModule {
-    constructor(private matIconRegistry: MatIconRegistry, private dom: DomSanitizer) {
-        matIconRegistry.addSvgIconSet(dom.bypassSecurityTrustResourceUrl('assets/mdi.svg'))
-            .addSvgIcon('website-logo', dom.bypassSecurityTrustResourceUrl('assets/website.svg'))
-            .addSvgIcon('website-logo-full', dom.bypassSecurityTrustResourceUrl('assets/website-logo-full.svg'));
-    }
+	constructor(private matIconRegistry: MatIconRegistry, private dom: DomSanitizer) {
+		matIconRegistry.addSvgIconSet(dom.bypassSecurityTrustResourceUrl('assets/mdi.svg'))
+			.addSvgIcon('website-logo', dom.bypassSecurityTrustResourceUrl('assets/website.svg'))
+			.addSvgIcon('website-logo-full', dom.bypassSecurityTrustResourceUrl('assets/website-logo-full.svg'));
+	}
 }
