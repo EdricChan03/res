120a893a896985b0555f98ac7dcc31efbc0c6b61
Edric Chan <edric.chan.1997@gmail.com>
2018-12-09 20:18:36 +0800

    chore(HomeComponent): remove `this`

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class HomeComponent implements OnInit {
 
   constructor(private shared: SharedService) {
-    this.shared.title = 'Home';
+    shared.title = 'Home';
   }
 
   ngOnInit() {
